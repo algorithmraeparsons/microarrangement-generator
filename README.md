bf1d98543e985fd9ef468e16a9fb038723743746
Algorithm Rae Parsons <algorithmraeparsons@gmail.com>
2025-02-16 20:46:31 -0800

    User input update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [Microarrangement Generator](https://algorithmraeparsons.github.io/microarrangement-generator)
 
-A website that randomly generates partition arrangement ideas within musical measures.
+A website that randomly generates partition arrangement ideas for a desired number of beats.
--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-// disallow negatives
-// disallow zero
-// disallow one
-
+// Validates beat input. Disallows negatives, 0, 1, and decimals.
+// Returns error message if problems, empty string otherwise.
 function validateBeats(beats){
     let message = "";
-    let isValid = false; // !!!
-    if (beats > 0){
-        message = "Number of beats per measure cannot be negative."
+    if (beats < 0){
+        message = "Number of beats cannot be negative."
     } else if (beats == 0){
-        message = "Number of beats per measure cannot be zero.";
+        message = "Number of beats cannot be 0.";
     } else if (beats == 1){
-        message = "Number of beats per measure cannot be one."
+        message = "Number of beats cannot be 1."
     } else if (beats%1 != 0){ // Checks if beats is not an integer.
-        message = "Number of beats per measure cannot have decimal places."
+        message = "Number of beats cannot have decimal places."
     } else { // valid input
         message = "";
     }
+    return message;
 }
 
-// Creates random string of 0s and 1s, the length of the random decimal.
+// Creates random string of 0s and 1s.
+// Returns binary string of decimal length
 function decimalToBinaryString(decimal){
-    let binaryString = "";
+    let binary = "";
     for (let i = 0; i < decimal; i++){
-        binaryString += Math.round(Math.random());
+        binary += Math.round(Math.random());
     }
-    return binaryString;
+    return binary;
 }
 
-
+// Converts random binary String to a parts array.
+// Returns array (possibly with empty strings).
 // 1 = +, added together
 // 0 = -, separated, looks like + on site
+// ex. beats = 4, random binary = 101/+-+, then parts = 1+1-1+1
 function binarytoPartsArray(binary){
     let partsInOnes = "";
     let length = binary.length;
     for (let i = 0; i < length; i++){
         if (binary[i] == 1){
-            partsInOnes += "1+"; // equiv to -
+            partsInOnes += "1+"; // equivalent to -
         } else {
-            partsInOnes += "1"; // equiv to +
+            partsInOnes += "1"; // equivalent to +
         }
     }
-
     const parts = partsInOnes.split("+");
     for (let i = 0; i < parts.length; i++){
         let num = parts[i].length;
@@ -50,6 +50,8 @@ function binarytoPartsArray(binary){
     return parts;
 }
 
+// Refines parts by removinng empty string elements.
+// Returns array.
 function removeEmptyParts(parts){
     for (let i = 0; i < parts.length; i++){
         if (parts[i] == ""){
@@ -59,6 +61,8 @@ function removeEmptyParts(parts){
     return parts;
 }
 
+// Converts parts array to string for printing.
+// Returns string.
 function partsToPartsString(parts){
     let partsString = "";
     for (let i = 0; i < parts.length; i++){
@@ -70,10 +74,12 @@ function partsToPartsString(parts){
     return partsString;
 }
 
-function generateRandomPartition(maxParts){
-    let randomBinary = decimalToBinaryString(maxParts);
+// Generate a random integer partition of a sum of beats.
+// All listed output values will add up to input.
+// Returns string.
+function generate(beats){
+    let randomBinary = decimalToBinaryString(beats);
     let parts = binarytoPartsArray(randomBinary);
     parts = removeEmptyParts(parts);
-    let partsString = partsToPartsString(parts);
-    return partsString;
+    return partsToPartsString(parts);
 }
